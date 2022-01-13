5cd84594d3ce38f92dba8fb7889c256abf4a22aa
Facundo Castro <facucastrolp123@gmail.com>
2022-01-13 01:36:07 -0300

    Correccion de errores

--- src/main/java/com/proyect/jef/controladores/PerfilControlador.java
+++ src/main/java/com/proyect/jef/controladores/PerfilControlador.java
@@ -8,26 +8,26 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
-@RequestMapping("/")
 public class PerfilControlador {
 
     @Autowired
     private PerfilDesarrolladorServicio servicioPerfil;
     
-    @GetMapping("/creacionPerfil")
+    @GetMapping
+    @RequestMapping("/crear_desarrollador")
     public String creacionDesarrollador(){
-        return "CrearPerfil.html";
+        return "CrearPerfilDesarrollador.html";
     }
-    
-    @PostMapping("/creacionPerfil")
+
+
+
+    @PostMapping
+    @RequestMapping("/creacion_desarrollador")
     public String crearDesarrollador(@RequestParam String nombre,@RequestParam String link,@RequestParam  String descripcion,@RequestParam(required = false)  MultipartFile archivo){
         try
         {
@@ -36,12 +36,41 @@ public class PerfilControlador {
         } catch (ErrorJef ex)
         {
             System.out.println(ex.getMessage());
-            return "CrearPerfil.html";
+            return "redirect:/creacion_desarrollador";
         } catch (IOException ex)
         {
             System.out.println(ex.getMessage());
-            return "CrearPerfil.html";
+            return "redirect:/creacion_desarrollador";
         }
     }
-    
+
+    @PutMapping
+    @RequestMapping("/editar_desarrollador")
+    public String editarDesarrollador(String idPerfil, @RequestParam(required = false) String nombre,@RequestParam(required = false) String link,@RequestParam(required = false)  String descripcion,@RequestParam(required = false)  MultipartFile archivo) {
+        try {
+            servicioPerfil.actualizarPerfilDesarrollador(idPerfil, nombre, link, descripcion, archivo);
+            return "Index.html";
+        } catch (ErrorJef ex) {
+            return "redirect:/editar_desarrollador";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "redirect:/editar_desarrollador";
+        }
+    }
+
+    @DeleteMapping
+    @RequestMapping("/eliminar_perfil")
+    public String borrarPerfil(@RequestParam String idPerfil){
+        try {
+            servicioPerfil.eliminarPerfilDesarrollador(idPerfil);
+            return "Index.html";
+        } catch (ErrorJef e) {
+            e.printStackTrace();
+            return "Index.html";
+        }
+    }
 }
+
+
+    
+
